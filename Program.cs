bfe95a8a8604bda37da8e1589b00c6b34ac030da
9kongpob <k.kunchanuan@hotmail.com>
2019-11-14 14:44:03 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BasicPart01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = 1995;
+            string name = "Kongpop Kunchanuan";
+            double points = 4.00;
+            bool status = true;
+            year = year + 543;
+            string str = "400.59";
+            double number;
+            double.TryParse(str,out number);
+            number = number + 6000;
+
+            Console.WriteLine("My name is " + name);
+            Console.WriteLine("Grade : " + points);
+            Console.WriteLine("Years : " + (year+10));
+            Console.WriteLine("Status : " + status);
+            Console.WriteLine("Result : " + number);
+            Console.ReadKey();
+        }
+    }
+}
